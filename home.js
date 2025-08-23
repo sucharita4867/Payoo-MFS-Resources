87f4310001ad231df50c5402d6459db4d0c78ace
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-23 23:33:06 +0530

    added this project task

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// add money feature
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -6,10 +7,55 @@ document
     const accountNo = document.getElementById("account-no").value;
     const addAmount = parseInt(document.getElementById("add-amount").value);
     const pinNO = document.getElementById("pin-no").value;
-    //     console.log(BankName, accountNo, addAmount, pinNO);
     const firstBalance = parseInt(document.getElementById("Balance").innerText);
-    //     console.log(firstBalance);
+    const validPin = "2006";
+    console.log(firstBalance);
+    if (accountNo.length < 11) {
+      alert("please provide valid account number");
+      return;
+    }
+    if (pinNO != validPin) {
+      alert("please provide valid pin number");
+      return;
+    }
     let newBalance = addAmount + firstBalance;
-    console.log(newBalance);
+    //     console.log(addAmount, firstBalance);
     document.getElementById("Balance").innerText = newBalance;
   });
+// cashOut feature
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const withdrawAmount = parseInt(
+    document.getElementById("withdraw-amount").value
+  );
+  const availableBalance = parseInt(
+    document.getElementById("Balance").innerText
+  );
+  const pinDisit = document.getElementById("pin-digit").value;
+  const agentNumber = document.getElementById("agent-number").value;
+  const cashOutPin = "2006";
+  if (agentNumber.length < 11) {
+    alert("please provide valid account number");
+    return;
+  }
+  if (pinDisit !== cashOutPin) {
+    alert("please provide valid pin number");
+    return;
+  }
+
+  const withdrawValue = withdrawAmount - availableBalance;
+  document.getElementById("Balance").innerText = withdrawValue;
+});
+
+// toggling feature
+document.getElementById("add-btn").addEventListener("click", function () {
+  document.getElementById("cash-out-parent").style.display = "none";
+  document.getElementById("add-money-parent").style.display = "block";
+});
+document.getElementById("cash-out-btn").addEventListener("click", function () {
+  document.getElementById("add-money-parent").style.display = "none";
+  document.getElementById("cash-out-parent").style.display = "block";
+});
+
+// document.querySelector("input").value;
